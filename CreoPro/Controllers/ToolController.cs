498da5f6710212ab6bf32e6e348efa4f82c399a7
haishangqishi <ahaishangqishi@163.com>
2017-01-12 22:12:08 +0800

    2017-01-12:大概设计机床、刀具界面。

---
+++ CreoPro/Controllers/ToolController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreoPro.Controllers
+{
+    public class ToolController : Controller
+    {
+
+        #region 页面跳转
+        /// <summary>
+        /// 刀具类型
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult toolType()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 刀具参数设置
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult toolSet()
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
---
+++ CreoPro/Scripts/toolType.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    //刀具类型（折叠框效果）
+    $('#toolList_aMin').click(function () {
+        var divcss = $('#toolListDiv').attr("style").toString();
+        if (divcss.indexOf("block") >= 0) {
+            $("#toolListDiv").slideToggle(500);
+            $('#toolList_iMin').removeClass("glyphicon-chevron-up");
+            $('#toolList_iMin').addClass("glyphicon-chevron-down");
+        } else {
+            $("#toolListDiv").show(500);
+            $('#toolList_iMin').removeClass("glyphicon-chevron-down");
+            $('#toolList_iMin').addClass("glyphicon-chevron-up");
+        }
+    });
+
+});
