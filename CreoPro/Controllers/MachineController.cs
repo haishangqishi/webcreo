19a87f86f91c149f94c5b5aa72c6caa5c022a15d
haishangqishi <ahaishangqishi@163.com>
2017-01-12 20:16:58 +0800

    2017-01-12:机床列表页面

---
+++ CreoPro/Controllers/MachineController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreoPro.Controllers
+{
+    public class MachineController : Controller
+    {
+
+        #region 页面跳转
+        /// <summary>
+        /// 机床类型
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult machineType()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 机床参数设置
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult machineSet()
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
